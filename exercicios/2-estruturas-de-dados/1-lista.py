f300e4952284b886466945d9244cb687ba0be0b2
FelipeANX <168134056+FelipeANX@users.noreply.github.com>
2024-05-30 14:57:15 +0000

    Iniciando resolução do exercico da lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,8 +1,10 @@
 # Crie uma lista apenas com elementos numéricos
-
+lista_1 = [1, 11, 22, 4, 5, 10, 55, 60]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista_2 = ['abacate', "feijão", 13, 3.14, 0, True, 3+4]
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print (lista_1[:5])
+print (lista_2[3:8])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
 
