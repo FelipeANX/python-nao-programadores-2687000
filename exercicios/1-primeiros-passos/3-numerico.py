5424bc3826d3d140e21c2f3791fd991a253c4896
FelipeANX <168134056+FelipeANX@users.noreply.github.com>
2024-05-30 14:19:35 +0000

    Reolução do exercicio 3

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type (data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+print (f"Quem nasce no ano de {data_nascimento} tem {idade_numerica} anos.")
+print (data_nascimento+f" {idade_numerica} anos")
+print (data_nascimento*idade_numerica)
 # Realize uma operação entre dados do tipo inteiro e float
+print (idade_numerica//altura)
+print (idade_numerica/altura)
+print (round((idade_numerica/altura),2))
