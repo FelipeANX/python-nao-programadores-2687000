17e27aa5a15cf91f368bb0a5f290ce1d8e72976b
FelipeANX <168134056+FelipeANX@users.noreply.github.com>
2024-05-31 20:01:10 +0000

    Resolução exercicios lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -7,9 +7,16 @@ print (lista_1[:5])
 print (lista_2[3:8])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+print (lista_1[0:-1:2])
 
 # Remova da lista o último item
+print (lista_1.pop())
 
 # Insira na lista um novo item
+print (lista_2.append(60))
 
 # Remova da lista um item específico
+print (lista_2.remove(3.14))
+
+print (lista_1)
+print (lista_2)
