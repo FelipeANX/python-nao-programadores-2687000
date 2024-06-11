6a1cbd343030d6fe34862292165ee09a7a09af75
FelipeANX <168134056+FelipeANX@users.noreply.github.com>
2024-06-11 15:00:20 +0000

    resolução?

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -6,11 +6,12 @@ estudante ['nome'] = input('Qual é o seu nome? ')
 estudante ['ano_faculdade'] = int(input('Em que ano você começou a facultade? '))
 estudante ['curso'] = input('Qual curso você fez? ')
 estudante ['ano_atual'] = int(input('Qual é o ano atual?'))
-estudante ['materias'] = input('Quais foram suas 5 matérias favoritas? (separe elas com vírgulas em ordem de favorita)')
+materias = input('Quais foram suas 5 matérias favoritas? (separe elas com vírgulas em ordem de favorita)')
 estudante ['formacao'] = int(input('Qual ano da sua formação?'))
 
 # 2. Armazene esses dados em um dicionário
 estudante ['materias'] = materias.split(', ')
 
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
-print(f'Oi, {estudante['nome']}, já estamos em {estudante["ano_atual"]} e fazem {estudante["ano_atual"]-estudante["formacao"]} anos que você se formou no curso de {estudante["curso"]}. Nos {estudante["formacao"]-estudante["ano_faculdade"]} anos de curso você gostou muito de ter feito as matérias de {estudante["materias"]}! Mas a favorita foi {estudante["curso"][0]}. Já parou para pensar no motivo dessa escolha?')
+
+print(f"Oi, {estudante['nome']}, já estamos em {estudante['ano_atual']} e fazem {estudante['ano_atual']-estudante['formacao']} anos que você se formou no curso de {estudante['curso']}. Nos {estudante['formacao']-estudante['ano_faculdade']} anos de curso você gostou muito de ter feito as matérias de {estudante['materias']}! Mas a favorita foi {estudante['materias'][0]}. Já parou para pensar no motivo dessa escolha?")
